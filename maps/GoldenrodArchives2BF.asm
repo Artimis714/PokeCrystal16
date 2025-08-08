58e9243a9336905f04d3285803629aed7c46f217
Artimis714 <archaeoastronomer63@gmail.com>
2025-08-09 00:40:46 +0100

    Archives Corrections
    Map Adjustments to the GoldenrodArchives Maps per the lessons learned in Map Testing

--- maps/GoldenrodArchives2BF.asm
+++ maps/GoldenrodArchives2BF.asm
@@ -11,8 +11,8 @@ GoldenrodArchives2BF_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 11, 13, GOLDENROD_ARCHIVES_1BF, 3
-	warp_event 14, 10, UNION_CAVE_B1F, 6
+	warp_event  5,  9, GOLDENROD_ARCHIVES_1BF, 3
+	warp_event 96,  5, UNION_CAVE_B1F, 6
 
 	def_coord_events
 
